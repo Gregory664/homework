d0f16cfdd0f4c1b2d33cb8212d0b2b9b7744af11
Gregory Belevskiy <gbelevskii@neoflex.ru>
2021-04-07 11:10:01 +0300

    Add module "calculator"

---
+++ calculator/src/main/java/ru/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.calculator;
+
+public class Calculator {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int min(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiple(int a, int b) {
+        return a * b;
+    }
+
+    public static double dib(int a, int b) {
+        return a / (b * 1D);
+    }
+}
